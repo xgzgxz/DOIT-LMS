2ddf576ef2172dbf20a06772868417de803cef08
xgz <lea.wolf15@gmail.com>
2025-10-31 17:27:50 +0100

    schneeflocke fix, ui polish, kommentare entfernt

--- src/views/impressum.php
+++ src/views/impressum.php
@@ -3,24 +3,26 @@
 // Sie bekommt vom Controller die $basePath-Variable.
 ?>
 
-<h1>Impressum</h1>
+<div class="static-content-wrapper">
+    <h1>Impressum</h1>
 
-<p>Hier stehen die rechtlichen Angaben gemäß § 5 TMG.</p>
+    <p>Hier stehen die rechtlichen Angaben gemäß § 5 TMG.</p>
 
-<h2>Anbieter:</h2>
-<p>
-    PinguWI<br>
-    c/o Max Mustermann (Dein Name)<br>
-    Musterstraße 1<br>
-    12345 Musterstadt
-</p>
+    <h2>Anbieter:</h2>
+    <p>
+        PinguWI<br>
+        c/o Max Mustermann (Dein Name)<br>
+        Musterstraße 1<br>
+        12345 Musterstadt
+    </p>
 
-<h2>Kontakt:</h2>
-<p>
-    Telefon: 01234 / 56789<br>
-    E-Mail: <a href="mailto:info@pinguwi.de">info@pinguwi.de</a>
-</p>
+    <h2>Kontakt:</h2>
+    <p>
+        Telefon: 01234 / 56789<br>
+        E-Mail: <a href="mailto:info@pinguwi.de">info@pinguwi.de</a>
+    </p>
 
-<p>
-    <em>(Dies ist ein Platzhalter-Impressum für ein Studienprojekt.)</em>
-</p>
+    <p>
+        <em>(Dies ist ein Platzhalter-Impressum für ein Studienprojekt.)</em>
+    </p>
+</div>
